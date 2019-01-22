e56f4ddddef5756fe889ba5a2d8d0378fc16496a
loachNW <2506234002@qq.com>
2019-01-22 16:47:15 +0800

    Initial commit

---
+++ test_model.py
@@ -0,0 +1,51 @@
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import Embedding, Dense, Bidirectional, Dropout, GRU,Conv1D,BatchNormalization,Activation,MaxPool1D,SimpleRNN
+from Attention import Attention
+from sklearn.externals import joblib
+
+
+
+num_words = 20000
+max_len =1200
+
+
+train_list = [i.strip() for i in open('C:/Users/ASUS/Desktop/train_test.txt', 'r', encoding='utf-8')]
+print(len(train_list))
+label_bj = [i.strip() for i in open('C:/Users/ASUS/Desktop/label_bj.txt', 'r', encoding='utf-8')]
+# tokenizer = Tokenizer(num_words = num_words)
+# tokenizer.fit_on_texts(train_list)
+tokenizer = joblib.load('tokenizer_final.model')
+
+train_list = tokenizer.texts_to_sequences(train_list)
+x_train = pad_sequences(train_list, max_len)
+lb = joblib.load("lb.model")
+
+
+model = Sequential([
+    Embedding(num_words + 1, 128, input_shape=(max_len,)),
+    Conv1D(128, 3, padding='same'),
+    BatchNormalization(),
+    Activation('relu'),
+    # MaxPool1D(10),
+    Bidirectional(GRU(128, return_sequences=True,reset_after=True), merge_mode='sum'),
+    Attention(128),
+    Dropout(0.5),
+    Dense(3, activation='softmax')
+])
+
+
+
+
+model.load_weights('model.h5')  #选取自己的.h模型名称
+count = 0
+pre = model.predict(x_train)
+pre = (pre > 0.5).astype('int32')
+pre = lb.inverse_transform(pre)
+for i in range(len(label_bj)):
+    if pre[i] == label_bj[i]:
+        count+=1
+print ('识别为：',count/174)
+pass
+
---
+++ web.py
@@ -0,0 +1,65 @@
+import re
+
+import jieba
+from flask import Flask, request, jsonify, render_template
+from keras.layers import Dense, BatchNormalization, Activation, Embedding, GRU,Conv1D,Bidirectional,Dropout
+from keras.models import Sequential
+from keras.preprocessing.sequence import pad_sequences
+from sklearn.externals import joblib
+from Attention import Attention
+
+restr = r'[0-9\s+\.\!\/_,$%^*();?:\-<>《》【】+\"\']+|[+——！，；。？：、~@#￥%……&*（）]+'
+
+
+
+max_len = 1200#每一条的长度
+num_words = 20000#
+
+class Region(object):
+    def __init__(self):
+        self.tokenizer = joblib.load('tokenizer_final.model')
+        self.lb = joblib.load('lb.model')
+        self.model = self.cnn_rnn_attention()
+
+    def cnn_rnn_attention(self):
+        model = Sequential([
+            Embedding(num_words + 1, 128, input_shape=(max_len,)),
+            Conv1D(128, 3, padding='same'),
+            BatchNormalization(),
+            Activation('relu'),
+            # MaxPool1D(10),
+            Bidirectional(GRU(128, return_sequences=True,reset_after=True), merge_mode='sum'),
+            Attention(128),
+            Dropout(0.5),
+            Dense(3, activation='softmax')
+        ])
+        model.load_weights('model.h5')
+        return model
+
+    def prdected(self, text):
+        resu = text.replace('|', '').replace('&nbsp;', '').replace('ldquo', '').replace('rdquo',
+                                                                                                          '').replace(
+            'lsquo', '').replace('rsquo', '').replace('“', '').replace('”', '').replace('〔', '').replace('〕', '')
+        resu = re.split(r'\s+', resu)
+        dr = re.compile(r'<[^>]+>', re.S)
+        dd = dr.sub('', ''.join(resu))
+        line = re.sub(restr, '', dd)
+        seg_list = jieba.lcut(line)
+        sequences = self.tokenizer.texts_to_sequences([seg_list])
+        data = pad_sequences(sequences, maxlen=1200)
+        pred = self.model.predict(data)
+        pred = (pred > 0.5).astype('int32')
+        return self.lb.inverse_transform(pred)
+model_obj = Region()
+app = Flask(__name__)
+
+@app.route("/",methods =["POST"])
+def index():
+    if request.method == "POST":
+        text = request.form.get("content")
+        if (text != "" and text != None):
+            result = {"result":model_obj.prdected(text)[0],"status":"1"}
+        else:
+            result = {"result":"DateType_error","status":"0"}
+        return jsonify(result)
+app.run(host = "0.0.0.0", port = 5005, debug = True, use_reloader = False)
